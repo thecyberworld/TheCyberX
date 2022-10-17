ac5d336cffa2f466888c0947ca727e6ce43e4bf2
mayank jain <mayankjain1394@gmail.com>
2022-10-17 19:33:22 +0530

    fix: fixed header styling (#7)
    * fix: fixed header styling
    
    * Update Navbar.tsx
    
    Co-authored-by: Kabir <44284877+kabir0x23@users.noreply.github.com>

--- src/components/Navbar/NavbarElements.tsx
+++ src/components/Navbar/NavbarElements.tsx
@@ -8,15 +8,17 @@ export const NavbarContainer = styled.div`
   justify-content: space-between;
   background: #2a2a2a;
   width: 100%;
-
-  height: 10%;
 `;
 
 export const LogoContainer = styled.div`
   width: 10%;
+  display: flex;
+  align-items: center;
 `;
 
 export const NavContent = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 export const Logo = styled.img`
@@ -28,5 +30,6 @@ export const GitHubIcon = styled.div`
   font-size: 1.5rem;
   display: flex;
   justify-content: center;
-  margin: 1rem 1rem 0 0;
+  align-items: center;
+  width: 10%;
 `;
