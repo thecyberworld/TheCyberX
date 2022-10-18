cc71ff05da906f90fe89f0c7f5c7ddd221001df4
Ritu Raj Singh <rrsingh1101@gmail.com>
2022-10-18 09:29:54 +0530

    feat: social handles (#12)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import {FooterContainer} from "./FooterElements";
 
-const Footer = () => {
+const Footer = (): JSX.Element => {
     return (
         <FooterContainer>
-            <p>Footer</p>
+            <div className='footer'>
+            <a href="https://twitter.com/thecyberw0rld" target="_blank"><i className="fa-brands fa-square-twitter"></i></a>
+            <a href="https://github.com/thecyberworld/support/issues/new?assignees=&labels=invite+me+to+the+organisation&template=invitation.yml&title=Please+invite+me+to+the+GitHub+Community+Organization" target="_blank"><i className="fa-brands fa-github"></i></a>
+            <a href="https://www.facebook.com/profile.php?id=100009345301794" target="_blank"><i className="fa-brands fa-facebook"></i></a>
+            <a href="https://www.linkedin.com/company/thecyberw0rld/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
+            <a href="https://www.instagram.com/thecyberw0rld" target="_blank"><i className="fa-brands fa-instagram"></i></a>
+            <a href="https://t.me/thecyberw0rld" target="_blank"><i className="fa-brands fa-telegram"></i></a>
+            <a href="https://discord.gg/QHBPq6xP5p" target="_blank"><i className="fa-brands fa-discord"></i></a>
+            </div>
         </FooterContainer>
     );
 };
--- src/components/Footer/FooterElements.tsx
+++ src/components/Footer/FooterElements.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const FooterContainer = styled.footer`
   position: absolute;
   bottom: 0;
-  margin: 0 20px;
-  display: flex;
+  width: 100%;
+  display: flex; 
   justify-content: center;
   align-items: center;
 `;
